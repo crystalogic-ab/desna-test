c106325ac90f7492154c017f9d7c872dee7a9c0c
none100p <andrewboltiansky@gmail.com>
2019-10-03 13:40:32 +0300

    removed router reducer

--- src/app/core/store/reducers/app.reducers.ts
+++ src/app/core/store/reducers/app.reducers.ts
@@ -1,11 +1,9 @@
 import { ActionReducerMap } from '@ngrx/store';
-import { routerReducer } from '@ngrx/router-store';
 import { AppState } from '../state/app.state';
 import { FirstReducer } from './first.reducers';
 import { SecondReducer } from './second.reducers';
 
 export const appReducers: ActionReducerMap<AppState, any> = {
-  router: routerReducer,
   first: FirstReducer,
   second: SecondReducer,
 };
